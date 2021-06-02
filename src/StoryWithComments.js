290149ee891083f6f8a1cd2ae6501c83860947b8
Ben Paget <ben.paget@live.fr>
2021-06-02 13:27:23 +0100

    Recursively display comments attached to a story

--- src/StoryWithComments.js
+++ src/StoryWithComments.js
@@ -11,20 +11,34 @@ import formatTime from "./utilities/formatTime";
 
 import arrow from "./img/grayarrow.gif";
 
-const Comment = ({ author, created_at }) => {
+const randomColor = () =>
+  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const Comment = ({ author, created_at, child, onUpvote }) => {
   return (
-    <div className="flex flex-col text-sm pt-1">
-      <div className="flex flex-row items-center">
-        <img
-          src={arrow}
-          alt="upvote"
-          style={{ width: "12px", height: "12px", marginBottom: "2px" }}
-        />
-        <p className="pl-1 text-hacker-dark text-xs">{author || "anonymous"}</p>
-        <p className="pl-1 text-hacker-dark text-xs">
-          {formatTime(created_at)}
-        </p>
-        <p className="pl-1 text-hacker-dark text-xs cursor-pointer">[-]</p>
+    <div className={`flex flex-col text-sm pt-1 ${child && "pl-4"}`}>
+      <div className="flex flex-col sm:flex-row items-center">
+        <div className="flex flex-row">
+          <img
+            src={arrow}
+            alt="upvote"
+            style={{ width: "12px", height: "12px", marginBottom: "2px" }}
+            className="cursor-pointer"
+            onClick={(e) => {
+              e.stopPropagation();
+              onUpvote();
+            }}
+          />
+          <p className="pl-1 text-hacker-dark text-xs">
+            {author || "anonymous"}
+          </p>
+        </div>
+        <div className="flex flex-row">
+          <p className="pl-1 text-hacker-dark text-xs">
+            {formatTime(created_at)}
+          </p>
+          <p className="pl-1 text-hacker-dark text-xs cursor-pointer">[-]</p>
+        </div>
       </div>
     </div>
   );
@@ -34,17 +48,26 @@ const CollapsibleComment = ({
   author,
   children,
   created_at,
-  id,
   text,
   onReply,
+  child,
+  onUpvote,
 }) => {
   if (!text) return null;
+  const hasChildren = children && children.length >= 1;
   return (
     <Collapsible
       open
-      trigger={<Comment author={author} created_at={created_at} />}
+      trigger={
+        <Comment
+          author={author}
+          created_at={created_at}
+          child={child}
+          onUpvote={onUpvote}
+        />
+      }
     >
-      <div className="pb-3">
+      <div>
         <div className="ml-6 text-xs pt-0.5">{parse(text)}</div>
         <span
           className="ml-6 text-hacker-dark text-xxs border-b border-dashed border-hacker-dark w-auto cursor-pointer"
@@ -52,37 +75,26 @@ const CollapsibleComment = ({
         >
           reply
         </span>
+        <div className="pl-4">
+          {hasChildren &&
+            children.map((c) => {
+              return (
+                <CollapsibleComment
+                  key={c.id}
+                  {...c}
+                  child
+                  onUpvote={onUpvote}
+                  onReply={onReply}
+                />
+              );
+            })}
+        </div>
       </div>
     </Collapsible>
   );
 };
 
-// author: "grouphugs"
-// children: []
-// created_at: "2021-05-25T16:25:31.000Z"
-// created_at_i: 1621959931
-// id: 27278857
-// options: []
-// parent_id: 27276706
-// points: null
-// story_id: 27276706
-// text: "<p>it&#x27;s probably time to stop recommending alt-right and fascist products, but you&#x27;re all literally nazis, and you&#x27;re going to die like ones</p>"
-// title: null
-// type: "comment"
-// url: null
-
-const getCommentsWithChildren = (comment) => {
-  // if(!children)
-  if (comment.children && comment.children.length) {
-    comment.children.map((comment) => {
-      return getCommentsWithChildren(comment);
-    });
-  }
-  return <CollapsibleComment key={comment.id} {...comment} />;
-};
-
 const StoryWithComments = () => {
-  // http://hn.algolia.com/api/v1/items/:id
   const { id } = useParams();
   const [error, loading, storyWithComments] = useHackerComments(id);
   console.log(storyWithComments);
@@ -90,6 +102,10 @@ const StoryWithComments = () => {
   const setScreenHeight = () => {
     if (!storyWithComments?.children?.length || loading) return "h-screen";
     return "h-full";
+  };
+
+  const handleUpvote = () => {
+    toaster("💁 You must be logged in to upvote a comment!");
   };
 
   const handleSubmitComment = (e) => {
@@ -114,14 +130,14 @@ const StoryWithComments = () => {
         <>
           <Story story={storyWithComments} withComments={true} />
           {/* Write a comment section */}
-          <div className="ml-6 pt-5 pr-6 md:pr-0">
+          <div className="ml-3 pt-5 mr-3 md:ml-6 md:mr-0">
             <form
-              className="flex flex-col md:w-2/3 lg:w-1/3 sm:w-full items-start"
+              className="flex flex-col w-full sm:w-full md:w-2/3 lg:w-1/3 md:items-start items-center"
               onSubmit={handleSubmitComment}
             >
               <textarea
-                className="border-black border"
-                cols="75"
+                className="border-black border w-full"
+                // cols="75"
                 rows="8"
               ></textarea>
               <button className="mt-4 bg-gradient-to-b from-gray-100 to-gray-200 border border-gray-500 p-0.5 pr-1 pl-1 font-normal text-sm">
@@ -134,11 +150,17 @@ const StoryWithComments = () => {
             {storyWithComments?.children?.length ? (
               storyWithComments.children.map((comment) => {
                 return (
-                  <CollapsibleComment
-                    key={comment.id}
-                    onReply={handleReplyToComment}
-                    {...comment}
-                  />
+                  <div
+                    className="pl-2 mb-3 border-l-2"
+                    style={{ borderColor: randomColor() }}
+                  >
+                    <CollapsibleComment
+                      key={comment.id}
+                      onReply={handleReplyToComment}
+                      onUpvote={handleUpvote}
+                      {...comment}
+                    />
+                  </div>
                 );
               })
             ) : (
@@ -154,5 +176,3 @@ const StoryWithComments = () => {
 };
 
 export default StoryWithComments;
-
-// getCommentsWithChildren(storyWithComments)
